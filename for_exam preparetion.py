aadd51205e7b7e961bf33514472d6a54b485bbde
Jogot <155011426+jogotpdas@users.noreply.github.com>
2024-12-15 20:37:25 +0600

    Add files via upload

---
+++ for_exam preparetion.py
@@ -0,0 +1,23 @@
+# a=int(input("enter the frist number="))
+# b=int(input("enter the second number="))
+#c=((a+b)/2)
+#print(c)
+#next code
+#chokro bridhi shudh
+#p=int(input("Enter the principal:"))
+#r=float(input("Enter the rate:"))
+#n=int(input("Enter the year:"))
+#c=p*(1+r/100)**n-1
+#print(c)
+#sorol shudh
+
+pp=int(input("Enter the principal:"))
+rr=int(input('Enter the rate:'))
+nn=int(input('Enter the year:'))
+i=int(pp*rr/100*nn)
+print(i)
+
+a=hi
+print(a)
+print(type,a)
+      
